a406c083206c0913cd0b59b3f4570baa2c6a2fb0
Meghana Nanuvala <71019344+meghanaNanuvala@users.noreply.github.com>
2026-06-14 17:52:55 -0400

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.py
@@ -0,0 +1,54 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
+
+        curSum = 0
+        maxArea = float("-inf")
+        visted = set()
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [(-1, 0),(1, 0), (0, -1), (0, 1)]
+
+        def bfs(r, c):
+            curSum = 1
+            queue = collections.deque()
+            visted.add((r,c))
+            queue.append((r,c))
+
+            while queue:
+                row, col = queue.popleft()
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r in range(ROWS) and 
+                    c in range(COLS) and
+                    grid[r][c]==1 and
+                    (r,c) not in visted):
+                        queue.append((r,c))
+                        visted.add((r,c))
+                        curSum += 1
+
+            return curSum
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c]==1 and (r,c) not in visted:
+                    # recursively run bfs on land
+                    curSum = bfs(r, c)
+                    maxArea = max(maxArea, curSum)
+
+        return max(maxArea, curSum)
+
+        
+
+
+                
+
+
+                
+
+
+
+        
+
+        
