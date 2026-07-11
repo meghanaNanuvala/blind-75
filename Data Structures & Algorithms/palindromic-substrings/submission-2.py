7d48fa08870496d870b567ef66d81f6b7e27ad1d
Meghana Nanuvala <71019344+meghanaNanuvala@users.noreply.github.com>
2026-07-11 19:16:13 -0400

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+
+        for i in range(len(s)):
+            # Odd length palindromes
+            res += self.countPalindrome(s, i, i)
+
+            # Even length palindromes
+            res += self.countPalindrome(s, i, i+1)
+            
+        return res
+
+    def countPalindrome(self, s, l, r):
+        res = 0
+        while l >=0 and r < len(s) and s[l]==s[r]:
+                res += 1
+                l -= 1
+                r += 1
+        return res
+        
